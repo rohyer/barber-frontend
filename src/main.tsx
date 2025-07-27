bf3c8b432bbc97a80c5a5d48fc335ea4a527a4e6
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-07-27 01:51:05 -0300

    chore: add ant-design support for react 19 and remove ConfigProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
+import '@ant-design/v5-patch-for-react-19';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { ConfigProvider } from 'antd';
-import { myTheme } from './design-system/theme/myTheme.ts';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ConfigProvider theme={myTheme}>
-            <App />
-        </ConfigProvider>
+        <App />
     </StrictMode>,
 );
