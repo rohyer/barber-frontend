298f854e16bfb14611d12413337ce571323b46c4
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-08-16 18:14:42 -0300

    Import Private and Public routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Register } from '../modules/auth/pages/Register';
 import { Login } from '../modules/auth/pages/Login';
+import { PrivateRoutes } from './PrivateRoutes';
+import { PublicRoutes } from './PublicRoutes';
+import MainLayout from '../layout/MainLayout';
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* <Route element={<PublicRoutes />}> */}
                 <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
+                <Route path='/login' element={<Login />}  />
+                {/* </Route> */}
+
+                <Route element={<PrivateRoutes />}>
+                    <Route path='/' element={<MainLayout />} />
+                </Route>
             </Routes>
         </BrowserRouter>  
     );
