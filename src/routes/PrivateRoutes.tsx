c38948bc61afec5d2c4efa72c6fa2773d4cc532b
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-08-18 22:04:16 -0300

    feat: create private and public routes component #1

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,8 +1,17 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../modules/auth/hooks/useAuth';
+import { Flex, Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 export function PrivateRoutes() {
-    const { isLoggedIn } = useAuth();
+    const { isLoggedIn, isLoading } = useAuth();
+
+    if (isLoading) 
+        return (
+            <Flex justify='center' align='center' style={{ height: '100vh' }}>
+                <Spin indicator={<LoadingOutlined />} size='large' />
+            </Flex>
+        );
 
     if (!isLoggedIn)
         return <Navigate to='/login' />;
