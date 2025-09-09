c461fa324fa4404f7f882161052d04f9b327a5ae
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-09-08 22:59:28 -0300

    feat: develop CreateClientModal component #6

--- src/modules/clients/components/CreateModal/CreateClientModal.tsx
+++ src/modules/clients/components/CreateModal/CreateClientModal.tsx
@@ -1,33 +1,20 @@
-import { Button, Calendar, DatePicker, Form, Input, message, Modal, Select, Space } from 'antd';
+import { DatePicker, Form, Input, message, Modal, Select } from 'antd';
 import { useState } from 'react';
 import { createClient } from '../../clients.service';
+import type { Clients } from '../../clients.type';
 
-type Props ={
+type Props = {
     isOpen: boolean,
     onCancel: () => void,
+    setClients: React.Dispatch<React.SetStateAction<Clients[]>>
 };
 
-export function CreateClientModal({ isOpen, onCancel }: Props) {
+export function CreateClientModal({ isOpen, onCancel, setClients }: Props) {
     const [form] = Form.useForm();
 
     const [isLoading, setIsLoading] = useState(false);
 
     const [error, setError] = useState<string | null>(null);
-
-    const footer = (
-        <Space>
-            <Button onClick={onCancel}>Voltar</Button>
-
-            <Button
-                type='primary'
-                htmlType='submit'
-                form="createClientModal"
-                loading={isLoading}
-            >
-                    Cadastrar
-            </Button>
-        </Space>
-    );
 
     const options = [
         {
@@ -47,15 +34,26 @@ export function CreateClientModal({ isOpen, onCancel }: Props) {
     const handleFinish = async (values: any) => {
         try {
             setIsLoading(true);
+
             setError(null);
 
-            const response = await createClient(values);
+            const payload = {
+                ...values,
+                birth: values.birth ? values.birth.toDate().toISOString().split('T')[0] : null,
+            };
+
+            const response = await createClient(payload);
+
+            onCancel();
+            
+            setIsLoading(false);
 
             message.open({
                 type: 'success',
-                content: 'Cliente cadastrado com sucesso!'
+                content: response.message,
             });
-            
+
+            setClients(prevClients => ([ ...prevClients, response.data]));            
         } catch(error) {
             setError(error instanceof Error ? error.message : 'Erro desconhecido');
         } finally {
@@ -67,8 +65,12 @@ export function CreateClientModal({ isOpen, onCancel }: Props) {
         <Modal
             title='Cadastrar cliente'
             open={isOpen}
+            okText="Cadastrar"
+            onOk={() => form.submit()}
+            okButtonProps={{ loading: isLoading }}
+            cancelText="Voltar"
             onCancel={onCancel}
-            footer={footer}
+            cancelButtonProps={{ disabled: isLoading }}
             destroyOnHidden
         >
             <Form
@@ -105,6 +107,7 @@ export function CreateClientModal({ isOpen, onCancel }: Props) {
                 <Form.Item
                     name='birth'
                     label='Data de nascimento'
+                    rules={[{ required: true, message: 'Preencha o campo Data de nascimento' }]}
                 >
                     <DatePicker
                         format='DD/MM/YYYY'
@@ -115,6 +118,7 @@ export function CreateClientModal({ isOpen, onCancel }: Props) {
                 <Form.Item
                     name='address'
                     label='Endereço'
+                    rules={[{ required: true, message: 'Preencha o campo Endereço' }]}
                 >
                     <Input />
                 </Form.Item>
