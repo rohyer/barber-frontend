bf66a1e2133f468f31284147e91ff8f998f6b145
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-09-23 23:50:34 -0300

    Chore: Create queryOptions to clients module #6

---
+++ src/modules/clients/clients.queries.ts
@@ -0,0 +1,24 @@
+import { queryOptions } from '@tanstack/react-query';
+import { getClients } from './clients.service';
+
+type Props = {
+    page: number,
+    search: string,
+}
+
+export const clientsQueryOptions = ({ page, search }: Props) => {
+    return queryOptions({
+        queryKey: ['clients', { page, search }],
+        queryFn: () => getClients(page, search),
+        staleTime: 1000 * 60,
+    });
+};
+
+export const searchClientsQueryOptions = ({ search }: Omit<Props, 'page'>) => {
+    return queryOptions({
+        queryKey: ['search-clients', { search }],
+        queryFn: () => getClients(0, search),
+        staleTime: 1000 * 60,
+        enabled: false,
+    });
+};
