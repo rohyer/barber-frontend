f951b2ecdefb80a7e0876688dfe24559f6860082
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-02-09 21:31:21 -0300

    Feat: Create action column

---
+++ src/modules/employees/components/Table/EmployeesActions..tsx
@@ -0,0 +1,54 @@
+import { Button, Space, theme, Tooltip } from 'antd';
+import { DeleteFilled, EditFilled } from '@ant-design/icons';
+import type { Dispatch, SetStateAction } from 'react';
+import type { EmployeeModel } from '../../employees.type';
+
+type Props = {
+    employee: EmployeeModel,
+    setIsUpdateModalOpen: Dispatch<SetStateAction<boolean>>
+    setUpdateEmployeeModal: Dispatch<SetStateAction<EmployeeModel | null>>
+    setIsDeleteModalOpen: Dispatch<SetStateAction<boolean>>
+    setDeleteEmployeeModal: Dispatch<SetStateAction<EmployeeModel | null>>
+}
+
+export function EmployeesActions({
+    employee,
+    setIsUpdateModalOpen,
+    setUpdateEmployeeModal,
+    setIsDeleteModalOpen,
+    setDeleteEmployeeModal,
+}: Props) {
+    const { token } = theme.useToken();
+
+    return (
+        <Space size='middle'>
+            <Tooltip title="Editar">
+                <Button
+                    shape='circle'
+                    type='text'
+                    onClick={() => {
+                        setIsUpdateModalOpen(true);
+                        setUpdateEmployeeModal(employee);
+                    }}
+                >
+                    <EditFilled
+                        style={{ color: token.colorPrimary }}
+                    />
+                </Button>
+            </Tooltip>
+
+            <Tooltip title="Deletar">
+                <Button 
+                    shape='circle'
+                    type='text'
+                    onClick={() => {
+                        setIsDeleteModalOpen(true);
+                        setDeleteEmployeeModal(employee);
+                    }}
+                >
+                    <DeleteFilled style={{ color: token.colorError }} />
+                </Button>
+            </Tooltip>
+        </Space>
+    );
+}
