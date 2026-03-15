19a9482b74321c18104722defb01eabc6b80cc21
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-14 23:39:21 -0300

    Initial development of type and contract for offerings module

---
+++ src/modules/offerings/offerings.contract.ts
@@ -0,0 +1,17 @@
+import type { OfferingModel } from './offerings.type';
+
+export type OfferingPayload = Pick<OfferingModel, 'name' | 'value' | 'duration'>;
+
+type Response<T> = {
+    success: boolean,
+    message: string,
+    fromCache: boolean,
+    data: T,
+}
+
+export type GetOfferings = {
+    response: Response<{
+        offerings: OfferingModel[],
+        total: number
+    }>
+}
---
+++ src/modules/offerings/offerings.type.ts
@@ -0,0 +1,6 @@
+export type OfferingModel = {
+    id: number,
+    name: string,
+    value: number,
+    duration: number,
+}
