bd94b76e5b7a839bc3134f0de3ade2521300649e
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-21 23:22:23 -0300

    Add employees prop type

--- src/modules/offerings/offerings.type.ts
+++ src/modules/offerings/offerings.type.ts
@@ -1,6 +1,12 @@
+type employees = {
+    id: number;
+    name: string;
+}
+
 export type OfferingModel = {
     id: number,
     name: string,
     value: number,
     duration: number,
+    employees: employees[],
 }
