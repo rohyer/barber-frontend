84cfc660efb45a5bcb7deaf60d0760bbaca910cd
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-24 22:10:36 -0300

    create useCreateOffering hook

---
+++ src/modules/offerings/useOfferings.ts
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { notify } from '../../shared/utils/notify';
+import type { FormInstance } from 'antd';
+import { createOffering } from './offerings.service';
+import type { CreateOffering } from './offerings.contract';
+
+type UseCreateOffering = {
+    form: FormInstance,
+    onCancel: () => void,
+}
+
+export const useCreateOffering = ({ form, onCancel }: UseCreateOffering) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (payload: CreateOffering['payload']) => createOffering(payload),
+        onSuccess: (response) => {
+            notify({ message: response.message });
+
+            queryClient.invalidateQueries({ queryKey: ['offerings'], exact: false });
+
+            onCancel();
+
+            form.resetFields();
+        },
+        onError: (error) => {
+            notify({
+                message: 'Erro ao cadastrar serviço',
+                description: error instanceof Error ? error.message : 'Erro desconhecido.',
+                type: 'error',
+            });
+        }
+    });
+    
+};
