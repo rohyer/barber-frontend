160c1bb7efe8414b064214572d68c0c8505b8df2
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-25 22:12:26 -0300

    list all employee options in select to be linked to offering

--- src/modules/offerings/components/modals/CreateOfferingModal.tsx
+++ src/modules/offerings/components/modals/CreateOfferingModal.tsx
@@ -1,6 +1,8 @@
-import { Flex, Form, Input, InputNumber, Modal } from 'antd';
+import { Flex, Form, Input, InputNumber, Modal, Select, type SelectProps } from 'antd';
 import { useCreateOffering } from '../../useOfferings';
 import type { OfferingPayload } from '../../offerings.contract';
+import { useQuery } from '@tanstack/react-query';
+import { employeeQueryOptions } from '../../offerings.queries';
 
 type Props = {
     isOpen: boolean,
@@ -10,7 +12,14 @@ type Props = {
 export function CreateOfferingModal({ isOpen, onCancel }: Props) {
     const [form] = Form.useForm();
 
+    const { data, isPending: isSelectPending } = useQuery(employeeQueryOptions());
+
     const { mutateAsync, isPending } = useCreateOffering({ form, onCancel });
+
+    const options: SelectProps['options'] = data?.data.employees.map(employee => ({
+        value: employee.id,
+        label: employee.name
+    }));
 
     const handleFinish = async (values: OfferingPayload) => {
         await mutateAsync(values);
@@ -61,6 +70,19 @@ export function CreateOfferingModal({ isOpen, onCancel }: Props) {
                         <InputNumber controls={false} style={{ width: '100%' }} />
                     </Form.Item>
                 </Flex>
+
+                <Form.Item
+                    name='employees'
+                    label='Colaboradores'
+                    style={{ width: '100%' }}
+                >
+                    <Select
+                        mode='multiple'
+                        options={options}
+                        loading={isSelectPending}
+                        allowClear
+                    />
+                </Form.Item>
                 
             </Form>
         </Modal>
