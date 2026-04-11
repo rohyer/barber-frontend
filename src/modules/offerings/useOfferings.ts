82852ee891208a917ddf2e04c099ed3b180721ad
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-11 00:05:10 -0300

    develop update hook

--- src/modules/offerings/useOfferings.ts
+++ src/modules/offerings/useOfferings.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { notify } from '../../shared/utils/notify';
 import type { FormInstance } from 'antd';
-import { createOffering } from './offerings.service';
+import { createOffering, updateOffering } from './offerings.service';
 import type { CreateOffering } from './offerings.contract';
 
 type UseCreateOffering = {
@@ -26,6 +26,30 @@ export const useCreateOffering = ({ form, onCancel }: UseCreateOffering) => {
         onError: (error) => {
             notify({
                 message: 'Erro ao cadastrar serviço',
+                description: error instanceof Error ? error.message : 'Erro desconhecido.',
+                type: 'error',
+            });
+        }
+    });
+};
+
+export const useUpdateOffering = ({ form, onCancel }: UseCreateOffering) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (payload: CreateOffering['payload']) => updateOffering(payload),
+        onSuccess: (response) => {
+            notify({ message: response.message });
+
+            queryClient.invalidateQueries({ queryKey: ['offerings'], exact: false });
+
+            onCancel();
+
+            form.resetFields();
+        },
+        onError: (error) => {
+            notify({
+                message: 'Erro ao atualizar serviço',
                 description: error instanceof Error ? error.message : 'Erro desconhecido.',
                 type: 'error',
             });
