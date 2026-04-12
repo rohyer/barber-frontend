dc79d9007ec3d2d44a2e2bef87e0f97955c3afb4
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-12 00:56:17 -0300

    move useOfferings of directory

--- src/modules/offerings/hooks/useOfferings.ts
+++ src/modules/offerings/hooks/useOfferings.ts
@@ -1,8 +1,8 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { notify } from '../../shared/utils/notify';
-import { createOffering, deleteOffering, updateOffering } from './offerings.service';
-import type { CreateOffering } from './offerings.contract';
-import type { OfferingModel } from './offerings.type';
+import { notify } from '../../../shared/utils/notify';
+import { createOffering, deleteOffering, updateOffering } from '../offerings.service';
+import type { CreateOffering, UpdateOffering } from '../offerings.contract';
+import type { OfferingModel } from '../offerings.type';
 
 export const useOfferingMutations = () => {
     const queryClient = useQueryClient();
@@ -26,7 +26,8 @@ export const useOfferingMutations = () => {
     });
 
     const { mutate: mutateUpdate, isPending: isUpdatePending } = useMutation({
-        mutationFn: (payload: CreateOffering['payload']) => updateOffering(payload),
+        mutationFn: ({ payload, offeringId }: {payload: UpdateOffering['payload'], offeringId: UpdateOffering['offeringId']}) =>
+            updateOffering(offeringId, payload),
         onSuccess: (response) => {
             notify({ message: response.message });
 
