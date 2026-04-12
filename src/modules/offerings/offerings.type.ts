5bc7228739cf98dd5e8c1e1135dbb514f0e0b3af
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-12 00:28:03 -0300

    develop OfferingFormValue

--- src/modules/offerings/offerings.type.ts
+++ src/modules/offerings/offerings.type.ts
@@ -1,4 +1,6 @@
-type employees = {
+import type { OFFERING_FORM_NAMES } from './offerings.constant';
+
+type Employees = {
     id: number;
     name: string;
 }
@@ -8,5 +10,12 @@ export type OfferingModel = {
     name: string,
     value: number,
     duration: number,
-    employees: employees[],
+    employees: Employees[],
+}
+
+export type OfferingFormValues = {
+    [OFFERING_FORM_NAMES.NAME]: string,
+    [OFFERING_FORM_NAMES.VALUE]: number,
+    [OFFERING_FORM_NAMES.DURATION]: number,
+    [OFFERING_FORM_NAMES.EMPLOYEE_IDS]: number[],
 }
