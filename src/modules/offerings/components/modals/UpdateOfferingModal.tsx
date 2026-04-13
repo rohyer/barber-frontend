fa4e6981dcbd8c4704ee561a7f3ae83a77f91533
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-13 00:02:29 -0300

    refactor UpdateOfferingModal

--- src/modules/offerings/components/modals/UpdateOfferingModal.tsx
+++ src/modules/offerings/components/modals/UpdateOfferingModal.tsx
@@ -1,8 +1,9 @@
 import { Flex, Form, Input, InputNumber, Modal, Select, Spin, type SelectProps } from 'antd';
 import { useQuery } from '@tanstack/react-query';
 import { employeeQueryOptions } from '../../offerings.queries';
-import type { OfferingModel } from '../../offerings.type';
-import { useOfferingMutations } from '../../useOfferings';
+import type { OfferingFormValues, OfferingModel } from '../../offerings.type';
+import { useOfferingMutations } from '../../hooks/useOfferings';
+import { OFFERING_FORM_NAMES } from '../../offerings.constant';
 
 type Props = {
     isOpen: boolean,
@@ -19,7 +20,7 @@ export function UpdateOfferingModal({
     setUpdateOfferingSelected,
     setIsUpdateModalOpen,
 }: Props) {
-    const [form] = Form.useForm();
+    const [form] = Form.useForm<OfferingFormValues>();
 
     const handleCancel = () => {
         setIsUpdateModalOpen(false);
@@ -34,6 +35,11 @@ export function UpdateOfferingModal({
         value: employee.id,
         label: employee.name
     }));
+
+    const parsedEmployees = updateOfferingSelected.employees.map(employee => ({
+        label: employee.name,
+        value: employee.id
+    }));    
 
     return (
         <Modal
@@ -51,11 +57,11 @@ export function UpdateOfferingModal({
                 form={form}
                 id="updateOfferingForm"
                 layout='vertical'
-                initialValues={{ ...updateOfferingSelected }}
-                onFinish={(values) => mutateUpdate(values)}
+                initialValues={{ ...updateOfferingSelected, employeeIds: parsedEmployees }}
+                onFinish={(values) => mutateUpdate({ payload: values, offeringId: updateOfferingSelected.id })}
             >
                 <Form.Item
-                    name='name'
+                    name={OFFERING_FORM_NAMES.NAME}
                     label='Nome'
                     rules={RULES}
                 >
@@ -64,7 +70,7 @@ export function UpdateOfferingModal({
 
                 <Flex justify='space-between' gap='small'>
                     <Form.Item
-                        name='value'
+                        name={OFFERING_FORM_NAMES.VALUE}
                         label='Valor'
                         rules={RULES}
                         style={{ width: '50%' }}
@@ -73,7 +79,7 @@ export function UpdateOfferingModal({
                     </Form.Item>
 
                     <Form.Item
-                        name='duration'
+                        name={OFFERING_FORM_NAMES.DURATION}
                         label='Duração'
                         rules={RULES}
                         style={{ width: '50%' }}
@@ -83,7 +89,7 @@ export function UpdateOfferingModal({
                 </Flex>
 
                 <Form.Item
-                    name='idEmployees'
+                    name={OFFERING_FORM_NAMES.EMPLOYEE_IDS}
                     label='Colaboradores'
                     rules={RULES}
                     style={{ width: '100%' }}
