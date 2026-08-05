321d984ff90d40600bfb64c1811cdbd23c059efe
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-08-04 22:23:48 -0300

    feat: develop AuthProvider #10

--- src/modules/auth/context/AuthProvider.tsx
+++ src/modules/auth/context/AuthProvider.tsx
@@ -2,35 +2,45 @@ import { useCallback, useEffect, useState, type ReactNode } from 'react';
 import { AuthContext } from './AuthContext';
 import type { Credentials, User } from '../auth.type';
 import { getMe, loginUser } from '../auth.service';
+import { logoutUser } from '../auth.service';
 
 type Props = {
     children: ReactNode
 };
 
-const USER_STORAGE_KEY = 'user';
-
 export function AuthProvider({ children }: Props) {
     const [user, setUser] = useState<User | undefined>(undefined);
 
     const [isLoading, setIsLoading] = useState(true);
-    
+
     useEffect(() => {
         const fetchMe = async() => {
             setIsLoading(true);
-
+    
             const response = await getMe();
 
+            if (!response.success) {
+                setUser(undefined);
+                setIsLoading(false);
+
+                return;
+            }
+    
             const { data } = response;
-
+    
             setUser(data?.data);
-
+    
             setIsLoading(false);
         };
-        
+            
         fetchMe();
-    }, []);
+    }, [setIsLoading, setUser]);
 
-    const logout = useCallback(() => {
+    const logout = useCallback(async () => {
+        const response = await logoutUser();
+
+        console.log(response);
+
         setUser(undefined);
     }, []);
 
