4a0346dce4ae32a04daf54c0758811c8567a28e3
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-08-04 22:24:02 -0300

    fix: add loading state #10

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,8 +1,17 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../modules/auth/hooks/useAuth';
+import { Flex, Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 export function PublicRoutes() {
-    const { user } = useAuth();
+    const { isLoading, user } = useAuth();
+
+    if (isLoading)
+        return (
+            <Flex justify='center' align='center' style={{ height: '100vh' }}>
+                <Spin indicator={<LoadingOutlined />} size='large' />
+            </Flex>
+        );
 
     if (user !== undefined)
         return <Navigate to="/" />;
