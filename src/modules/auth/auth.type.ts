2622f6b0c617d8b2d8931bc3669d2aa2aa649336
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-08-06 20:28:05 -0300

    chore: comment types #10

--- src/modules/auth/auth.type.ts
+++ src/modules/auth/auth.type.ts
@@ -1,16 +1,18 @@
+import type { Dispatch, SetStateAction } from "react"
+
 export type User = {
-    id: number,
+    // id: number,
     name: string,
     email: string,
     state: string,
     city: string,
     phone: string,
-    status: 0 | 1,
-    createdAt: string | null,
-    emailToken: string | null,
-    emailTokenExpires: string |null,
-    newEmail: string | null,
     premiumExpiresAt: string | null,
+    // status: 0 | 1,
+    // createdAt: string | null,
+    // emailToken: string | null,
+    // emailTokenExpires: string |null,
+    // newEmail: string | null,
 }
 
 export type Credentials = {
@@ -19,7 +21,7 @@ export type Credentials = {
 }
 
 export type AuthContextType = {
-    user: User | null,
+    user?: User,
     isLoading: boolean,
     login: (credentials: Credentials) => Promise<boolean | undefined>,
     logout: () => void,
