ff22efe7254e1c165de66488267f095fe7b6914e
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-08-14 22:41:34 -0300

    fix: imports #12

--- src/features/auth-register/api/authRegisterUser.ts
+++ src/features/auth-register/api/authRegisterUser.ts
@@ -1,5 +1,5 @@
-import type { RegisterUser } from "../../../modules/auth/auth.contract";
-import { apiClient } from "../../../shared/services/api.service";
+import type { RegisterUser } from '../model/authRegisterUser.type';
+import { apiClient } from '../../../shared/services/api.service';
 
 export const authRegisterUser = (body: RegisterUser['body']) => {
     const url = 'http://localhost:80/api/auth/register';
