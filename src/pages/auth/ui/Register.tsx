06482b5f2b8c666ed6a19e5a0ece274b783cdc82
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-08-14 22:39:21 -0300

    fix: remove not necessary imports #12

--- src/pages/auth/ui/Register.tsx
+++ src/pages/auth/ui/Register.tsx
@@ -1,8 +1,4 @@
-import { Button, Card, Col, Flex, Form, Input, Row, Select, Space, message } from 'antd';
-import { Typography } from 'antd';
-// import type { FormProps } from 'antd';
-import { authRegisterUser } from '../../../features/auth-register/api/authRegisterUser';
-import { useNavigate } from 'react-router-dom';
+import { Card, Flex } from 'antd';
 import style from './Register.module.css';
 import { AuthRegisterForm } from '../../../features/auth-register/ui/AuthRegisterForm';
 
